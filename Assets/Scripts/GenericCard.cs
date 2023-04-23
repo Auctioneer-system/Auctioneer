8dc29f83d4b3940b95ff5d46901ab61d69cf9b2d
tea-creats <annihilation0504@gmail.com>
2023-04-23 12:07:53 +0900

    mobile build

--- Assets/Scripts/GenericCard.cs
+++ Assets/Scripts/GenericCard.cs
@@ -10,7 +10,8 @@ public class GenericCard : MonoBehaviour {
 
 	public int cardIndex;
 	public int tailIndex;
-	static float time = 8f;
+	static float velocity = 8f;
+	// 時間に余裕があればVelocity関数の定義が欲しい
 
 	RectTransform rectTransform;
 	public bool isFace;
@@ -20,8 +21,8 @@ public class GenericCard : MonoBehaviour {
 		Vector3 startScale = new Vector3(1f, 1f, 1f);
 		Vector3 endScale = new Vector3(1f, 0f, 1f);
 		Vector3 localScale = new Vector3();
-		while (tick < time/2){
-			tick += Time.deltaTime * time;
+		while (tick < velocity/2){
+			tick += Time.deltaTime * velocity;
 			localScale = Vector3.Lerp(startScale, endScale, tick);
 			rectTransform.localScale = localScale;
 			yield return null;
@@ -35,8 +36,8 @@ public class GenericCard : MonoBehaviour {
 			
 		}
 		isFace = !isFace;
-		while (tick < time){
-			tick += Time.deltaTime * time;
+		while (tick < velocity){
+			tick += Time.deltaTime * velocity;
 			localScale = Vector3.Lerp(startScale, endScale, tick);
 			rectTransform.localScale = localScale;
 			yield return null;
@@ -57,4 +58,11 @@ public class GenericCard : MonoBehaviour {
 	public void StartTurn(){
 		StartCoroutine(ToggleImage());
 	}
+	private void setCardImage(int face, int tail){
+		cardImage.sprite = face_images[face < face_images.Length-1 ? face : 0];
+		cardImage.sprite = tail_images[tail < tail_images.Length-1 ? tail : 0];
+
+
+	}
+	
 }
